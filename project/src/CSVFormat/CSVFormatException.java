210363716c459f386e36bd630d1464788e9d74ef
Gary N. Bagnall <gbagnall8@gmail.com>
2018-04-29 12:43:01 +1000

    Added Exception classes

---
+++ project/src/CSVFormat/CSVFormatException.java
@@ -0,0 +1,29 @@
+package CSVFormat;
+
+public class CSVFormatException extends Exception {
+
+	public CSVFormatException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CSVFormatException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CSVFormatException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CSVFormatException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CSVFormatException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ project/src/stock/StockException.java
@@ -0,0 +1,29 @@
+package stock;
+
+public class StockException extends Exception {
+
+	public StockException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public StockException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StockException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StockException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StockException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
