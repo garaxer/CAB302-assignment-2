44a84e29a03003cf49ecee0e79dde6973979ae92
Alex Koppon <alexkoppon@gmail.com>
2018-05-26 14:07:39 +1000

    Updated to reflect Truck

--- project/src/delivery/RefrigeratedTruckTest.java
+++ project/src/delivery/RefrigeratedTruckTest.java
@@ -1,6 +1,7 @@
 package delivery;
 import stock.Item;
 import stock.Stock;
+import stock.StockException;
 
 import static org.junit.Assert.*;
 
@@ -31,7 +32,7 @@ public class RefrigeratedTruckTest {
 	
 	// 1: Test if a RefrigeratedTruck object can be constructed
 	@Test
-	public void checkCreationwithStock() {
+	public void checkCreationwithStock() throws DeliveryException {
 		Stock stock = new Stock();
 		stock.addItems(chocolate, 10);
 		Truck refrigerated = new RefrigeratedTruck(stock);
@@ -39,7 +40,7 @@ public class RefrigeratedTruckTest {
 	
 	// 2: Test of Stock can be added
 	@Test
-	public void checkAddingStock() {
+	public void checkAddingStock() throws DeliveryException {
 		Truck refrigerated = new RefrigeratedTruck();
 		Stock stock = new Stock();
 		refrigerated.addStock(stock);		
@@ -47,7 +48,7 @@ public class RefrigeratedTruckTest {
 	
 	// 3: Test 
 	@Test
-	public void checkStock() {
+	public void checkStock() throws DeliveryException, StockException {
 		Stock stock = new Stock();
 		stock.addItems(ice);
 		Truck refrigerated = new RefrigeratedTruck();
@@ -62,32 +63,30 @@ public class RefrigeratedTruckTest {
 		stock.addItems(ice, 1200);
 	}
 	
-	@SuppressWarnings("deprecation")
 	@Test
-	public void checkCost(){
+	public void checkCost() throws DeliveryException, StockException{
 		Stock stock = new Stock();
 		stock.addItems(ice);
 		int temp = ice.getItemTemperature();
 		Truck refrigerated = new RefrigeratedTruck();
 		refrigerated.addStock(stock);
 		double cost = (900.0 + 200.0*Math.pow(0.7, (temp/5)));
-		assertEquals(cost, refrigerated.getCost());
+		assertEquals(cost, refrigerated.getCost(), 0);
 	}
 	
-	@SuppressWarnings("deprecation")
-	public void checkCost2() {
+	@Test
+	public void checkCost2() throws StockException, DeliveryException {
 		Stock stock = new Stock();
 		stock.addItems(iceCream);
 		int temp = iceCream.getItemTemperature();
 		Truck refrigerated = new RefrigeratedTruck();
 		refrigerated.addStock(stock);
 		double cost = (900.0 + 200.0*Math.pow(0.7, (temp/5)));
-		assertEquals(cost, refrigerated.getCost());
+		assertEquals(cost, refrigerated.getCost(), 0);
 	}
-	
-	
+		
 	@Test
-	public void checkCapacity() {	 
+	public void checkCapacity() throws DeliveryException {	 
 		Stock stock = new Stock();
 		stock.addItems(chocolate, 700);
 		Truck refrigerated = new RefrigeratedTruck();
@@ -97,19 +96,19 @@ public class RefrigeratedTruckTest {
 	
 	
 	@Test
-	public void checkCapacity2() {	 
+	public void checkCapacity2() throws DeliveryException {	 
 		Stock stock = new Stock();
-		stock.addItems(chocolate, 700);
+		stock.addItems(chocolate, 100);
 		Truck refrigerated = new RefrigeratedTruck();
 		refrigerated.addStock(stock);
 		Stock stock2 = new Stock();
 		stock2.addItems(chocolate, 300);
-		refrigerated.removeStock(stock2);
+		refrigerated.addStock(stock2);
 		assertEquals(400, refrigerated.getRemainingCapacity());
 	}
 	
 	@Test
-	public void checkManifest() {
+	public void checkManifest() throws DeliveryException, StockException {
 		String dummyManifest = ">Refrigerated\nchocolate,50\nice,400\n";
 		String dummyManifest2 = ">Refrigerated\nice,400\nchocolate,50\n";
 		Stock stock = new Stock();
@@ -117,7 +116,7 @@ public class RefrigeratedTruckTest {
 		stock.addItems(ice, 500);
 		Truck truck = new RefrigeratedTruck();
 		truck.addStock(stock);
-		assertTrue((dummyManifest == truck.getManifest() || dummyManifest2 == truck.getManifest()));
+		assertTrue((dummyManifest.equals(truck.getManifest()) || dummyManifest2.equals(truck.getManifest())));
 		
 	}
 	
